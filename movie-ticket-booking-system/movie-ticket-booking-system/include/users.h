0609e6bf4dd851b183c19c6216cb5da4f8e40072
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2025-03-08 14:21:59 +0200

    Add the constructor for User and make the get and add methods for Email, Password, FirstName and LastName.

---
+++ movie-ticket-booking-system/movie-ticket-booking-system/include/users.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+using std::string;
+
+class User
+{
+public:
+	User(string Email, string Password, string FirstName, string LastName);
+	~User();
+
+	string GetEmail();
+	string GetPassword();
+	string GetFirstName();
+	string GetLastName();
+
+private:
+	string Email;
+	string Password;
+
+	string FirstName;
+	string LastName;
+
+	void SetEmail(string Email);
+	void SetPassword(string Password);
+	void SetFirstName(string FirstName);
+	void SetLastName(string LastName);
+};
--- movie-ticket-booking-system/movie-ticket-booking-system/src/Users.cpp
+++ movie-ticket-booking-system/movie-ticket-booking-system/src/Users.cpp
@@ -1,1 +1,54 @@
-#include "../include/users.hpp"
+#include "../include/users.h"
+
+
+User::User(string Email, string Password, string FirstName, string LastName)
+{
+	SetLastName(LastName);
+	SetFirstName(FirstName);
+	SetPassword(Password);
+	SetEmail(Email);
+}
+
+User::~User()
+{
+}
+
+void User::SetEmail(string Email)
+{
+	this->Email = Email;
+}
+
+void User::SetPassword(string Password)
+{
+	this->Password = Password;
+}
+
+void User::SetFirstName(string FirstName)
+{
+	this->FirstName = FirstName;
+}
+
+void User::SetLastName(string LastName)
+{
+	this->LastName = LastName;
+}
+
+string User::GetEmail()
+{
+	return this->Email;
+}
+
+string User::GetPassword()
+{
+	return this->Password;
+}
+
+string User::GetFirstName()
+{
+	return this->FirstName;
+}
+
+string User::GetLastName()
+{
+	return this->LastName;
+}
